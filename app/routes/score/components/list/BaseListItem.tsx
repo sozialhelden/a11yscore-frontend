f4a95ea104235fb17dc7bb72019e674d4da4eb61
Mayaryin <lynn.janzen@posteo.de>
2025-12-16 12:48:29 +0100

    feat: make arrow visible on hover

--- app/routes/score/components/list/BaseListItem.tsx
+++ app/routes/score/components/list/BaseListItem.tsx
@@ -18,14 +18,14 @@ export default function BaseListItem({
     <NavLink
       to={to}
       preventScrollReset={true}
-      className={`group flex gap-4 rounded justify-between items-center hover:bg-primary/20 px-5 py-1.5 border-2 border-transparent ${isActive ? "border-gray-600!" : ""}`}
+      className={`group flex gap-4 rounded justify-between items-center px-5 py-1.5 border-2 border-transparent ${isActive ? "border-gray-600!" : ""}`}
     >
       <span className="flex gap-4 items-center">
         <ScoreBox score={score} />
         {name}
       </span>
       <ArrowRight
-        className="group-hover:translate-x-1 duration-200 transition-transform"
+        className={`${isActive ? "opacity-100" : "opacity-0"} group-hover:opacity-100 group-hover:translate-x-1 group-focus:opacity-100 group-focus:translate-x-1 duration-500 transition-all`}
         size={20}
       />
     </NavLink>
